996a94581aee7e8217590670dcd5e6f5bbff7ef9
Beseda <adribeseda@gmail.com>
2024-11-07 07:19:13 +0100

    7.11

---
+++ 05_skladame_stranku_include/05_skladame_stranku_include/2024.11.7/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        
+    $polomer = 25;
+
+    $obvod = 2 * 3.14 * $polomer;
+    $obsah = 3.14 *($polomer * $polomer);
+
+    echo"Obvod je ". $obvod ." a obsah je ". $obsah;
+    ?>
+    
+</body>
+</html>
